2aa7550141dea7b5d4b6a9a54892cef2500c8d74
Isaac Virshup <ivirshup@gmail.com>
2023-07-03 15:38:26 +0200

    Fix deps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2.0',
     py_modules=['jp'],
     install_requires=[
-        'Click', "tabulate", "requests", "notebook>7"
+        'Click', "tabulate", "requests", "notebook>=7"
     ],
     entry_points={
         'console_scripts': [
